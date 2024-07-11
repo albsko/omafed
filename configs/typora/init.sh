eee25e16737a6090bc9c1767c18e3b4c1dcda8be
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-11 11:54:46 +0200

    Organize ./configs

---
+++ configs/typora/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ~/.config/Typora/themes
+cp ~/.local/share/omakub/configs/typora/ia_typora.css ~/.config/Typora/themes/
+cp ~/.local/share/omakub/configs/typora/ia_typora_night.css ~/.config/Typora/themes/
---
+++ configs/vscode/init.sh
@@ -0,0 +1,5 @@
+mkdir -p ~/.config/Code/User
+cp ~/.local/share/omakub/configs/vscode.json ~/.config/Code/User/settings.json
+
+# Install default supported themes
+code --install-extension enkia.tokyo-night

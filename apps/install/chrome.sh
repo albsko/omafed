220bdf540622fb91ced51fefcd27c85572fe3b29
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-11 11:09:43 +0200

    Update ./apps

--- apps/install/chrome.sh
+++ apps/install/chrome.sh
@@ -1,4 +1,6 @@
+#!/bin/bash
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
+
 cd /tmp
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install -y ./google-chrome-stable_current_amd64.deb

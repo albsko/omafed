f9f544f8a7f785e28296d65f781cfb61208cc66b
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-11 11:14:49 +0200

    Update apps.sh with logic to install ./apps/install/*.sh

--- apps.sh
+++ apps.sh
@@ -1,13 +1,21 @@
+#!/bin/bash
+
 # Ensure computer doesn't go to sleep or lock while installing
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 
+# Determine the directory of the script
+SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0" || echo "$0")")" && pwd)
+
 # Run desktop installers
-for installer in ~/.local/share/omakub/install/desktop/*.sh; do source $installer; done
+for installer in "$SCRIPT_DIR/apps/install"*.sh; do
+    echo "Running $installer"
+    bash "$installer"
+done
 
 # Revert to normal idle and lock settings
 gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.session idle-delay 300
 
-# Logout to pickup changes
-gum confirm "Ready to logout for all settings to take effect?" && gnome-session-quit --logout --no-prompt
+# gum confirm "Ready to logout for all settings to take effect?"
+# gnome-session-quit --logout --no-prompt

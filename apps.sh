b9ba75bd936891953753156825fdc237a8d5ac1d
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-12 17:31:13 +0200

    Update apps.sh with working gum choose

--- apps.sh
+++ apps.sh
@@ -5,10 +5,26 @@ gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 
 # Determine the directory of the script
-SCRIPT_DIR=$(cd "$(dirname "$(readlink -f "$0" || echo "$0")")" && pwd)
+RUNNING_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+APPS_DIR="$RUNNING_SCRIPT_DIR/apps/install/"
 
-# Run desktop installers
-for installer in "$SCRIPT_DIR/apps/install"*.sh; do
+# Find all .sh files in the APPS_DIR, sort them, and store them in an array
+sh_files=($(find "$APPS_DIR" -type f -name "*.sh" | sort))
+declare -A sh_files_map
+
+# Extract just the names of the scripts without the paths and store them in the associative array
+for file in "${sh_files[@]}"; do
+    sh_file_name=$(basename "$file" .sh)
+    sh_files_map["$sh_file_name"]="$file"
+done
+sorted_files_map_keys=($(for key in "${!sh_files_map[@]}"; do echo "$key"; done | sort))
+
+# Ask for apps to install
+apps_to_install=$(gum choose "${sorted_files_map_keys[@]}" --no-limit --height 20 --header "Select apps")
+
+# Run proper app installer scripts
+for app in $apps_to_install; do
+    installer="${sh_files_map[$app]}"
     echo "Running $installer"
     bash "$installer"
 done

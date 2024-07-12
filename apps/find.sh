9fe885665410c33268024b4c1f732211beeb1bf1
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-12 19:45:04 +0200

    Remove all find.sh

--- apps/find.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# TODO: find all app occurencies within apt or flatpak
-# create common app list from apt and flatpak, and grep on this with app names
-# retrieve app names from ./install/<filename>.sh

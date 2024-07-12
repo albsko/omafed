570c70e1d5aab9a09ff204a02563c413a888699c
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-12 12:14:01 +0200

    Update ./required

--- required/gum.sh
+++ required/gum.sh
@@ -1,6 +1,7 @@
-# Gum is used for the Omakub commands for tailoring Omakub after the initial install
+#!/bin/bash
+
 cd /tmp
-GUM_VERSION="0.14.1" # Use known good version
+GUM_VERSION="0.14.1"
 wget -qO gum.deb "https://github.com/charmbracelet/gum/releases/latest/download/gum_${GUM_VERSION}_amd64.deb"
 sudo apt-get install -y ./gum.deb
 rm gum.deb

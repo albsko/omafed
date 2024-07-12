5fa60eca60198fc492b47329e33b82c5ec96acbc
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-12 20:28:40 +0200

    Update apps.sh with mapfile implementation

--- apps.sh
+++ apps.sh
@@ -6,10 +6,10 @@ gsettings set org.gnome.desktop.session idle-delay 0
 
 # Determine the directory of the script
 RUNNING_SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-APPS_DIR="$RUNNING_SCRIPT_DIR/apps/install/"
+APPS_DIR="$RUNNING_SCRIPT_DIR/install/apps/"
 
-# Find all .sh files in the APPS_DIR, sort them, and store them in an array
-sh_files=($(find "$APPS_DIR" -type f -name "*.sh" | sort))
+# Find all .sh files in the APPS_DIR, sort them, and store them in an array using mapfile
+mapfile -t sh_files < <(find "$APPS_DIR" -type f -name "*.sh" | sort)
 declare -A sh_files_map
 
 # Extract just the names of the scripts without the paths and store them in the associative array
@@ -17,7 +17,9 @@ for file in "${sh_files[@]}"; do
     sh_file_name=$(basename "$file" .sh)
     sh_files_map["$sh_file_name"]="$file"
 done
-sorted_files_map_keys=($(for key in "${!sh_files_map[@]}"; do echo "$key"; done | sort))
+
+# Sort the keys of the associative array and store them in an array using mapfile
+mapfile -t sorted_files_map_keys < <(for key in "${!sh_files_map[@]}"; do echo "$key"; done | sort)
 
 # Ask for apps to install
 apps_to_install=$(gum choose "${sorted_files_map_keys[@]}" --no-limit --height 20 --header "What apps would you like to install?")
@@ -32,6 +34,3 @@ done
 # Revert to normal idle and lock settings
 gsettings set org.gnome.desktop.screensaver lock-enabled true
 gsettings set org.gnome.desktop.session idle-delay 300
-
-# gum confirm "Ready to logout for all settings to take effect?"
-# gnome-session-quit --logout --no-prompt

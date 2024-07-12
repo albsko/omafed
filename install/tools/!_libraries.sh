b2f4e949906579d0b1d815dd20fd689c9342be1b
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-12 22:07:14 +0200

    Update bang scripts in install/tools

--- install/tools/!_libraries.sh
+++ install/tools/!_libraries.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
+set -e
 
-sudo apt install -y \
-	build-essential pkg-config autoconf bison clang \
-	libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
-	libvips imagemagick libmagickwand-dev mupdf mupdf-tools \
-	redis-tools sqlite3 libsqlite3-0 libmysqlclient-dev
+pkgs=(
+	build-essential
+	pkg-config
+	autoconf
+	bison
+	clang
+	libssl-dev
+	libreadline-dev
+	zlib1g-dev
+	libyaml-dev
+	libreadline-dev
+	libncurses5-dev
+	libffi-dev
+	libgdbm-dev
+	libjemalloc2
+	libvips
+	imagemagick
+	libmagickwand-dev
+	mupdf
+	mupdf-tools
+	redis-tools
+	sqlite3
+	libsqlite3-0
+	libmysqlclient-dev
+)
+
+for pkg in "${pkgs[@]}"; do
+	if ! dpkg -l | grep -q "$pkg"; then
+		echo "Installing $pkg..."
+		sudo apt install -y "$pkg"
+	else
+		echo "$pkg is already installed."
+	fi
+done
--- install/tools/!_terminal.sh
+++ install/tools/!_terminal.sh
@@ -1,3 +1,34 @@
 #!/bin/bash
+set -e
 
-sudo apt install -y fzf ripgrep bat eza zoxide plocate btop apache2-utils fd-find
+pkgs=(
+    fzf
+    ripgrep
+    bat
+    zoxide
+    plocate
+    btop
+    apache2-utils
+    fd-find
+)
+
+for pkg in "${pkgs[@]}"; do
+    if ! dpkg -l | grep -q "$pkg"; then
+        echo "Installing $pkg..."
+        sudo apt install -y "$pkg"
+    else
+        echo "$pkg is already installed."
+    fi
+done
+
+if ! dpkg -l | grep -q "eza"; then
+    echo "Installing eza..."
+    sudo mkdir -p /etc/apt/keyrings
+    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o -n /etc/apt/keyrings/gierens.gpg
+    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    sudo apt update
+    sudo apt install -y eza
+else
+    echo "eza is already installed."
+fi

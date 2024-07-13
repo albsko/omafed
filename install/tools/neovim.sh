1abef1e47739e123ff85ef8d44fec9e1050e3ed1
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-13 15:57:06 +0200

    Fix install/tools

--- install/tools/neovim.sh
+++ install/tools/neovim.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cd /tmp
+cd /tmp || return
 wget -O nvim.tar.gz "https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz"
 tar -xf nvim.tar.gz
 sudo install nvim-linux64/bin/nvim /usr/local/bin/nvim
 sudo cp -R nvim-linux64/lib /usr/local/
 sudo cp -R nvim-linux64/share /usr/local/
 rm -rf nvim-linux64 nvim.tar.gz
-cd -
+cd - || return
--- install/tools/zellij.sh
+++ install/tools/zellij.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-cd /tmp
+cd /tmp || return
 wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
 tar -xf zellij.tar.gz zellij
 sudo install zellij /usr/local/bin
 rm zellij.tar.gz zellij
-cd -
-
+cd - || return

7e6fa08d5f2919745f1a0d9cb69735579a7b0c7c
Albert Skonieczny <albertskonieczny@gmail.com>
2024-08-12 18:37:47 +0200

    Add Inter font

--- install/desktop/fonts.sh
+++ install/desktop/fonts.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 mkdir -p ~/.local/share/fonts
 
-cd /tmp
+cd /tmp || return
 wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaMono.zip
 unzip CascadiaMono.zip -d CascadiaFont
 cp CascadiaFont/*.ttf ~/.local/share/fonts
@@ -11,5 +13,14 @@ unzip iafonts.zip -d iaFonts
 cp iaFonts/iA-Fonts-master/iA\ Writer\ Mono/Static/iAWriterMonoS-*.ttf ~/.local/share/fonts
 rm -rf iafonts.zip iaFonts
 
+INTER_ZIP_URL=$(curl -s https://api.github.com/repos/rsms/inter/releases/latest | grep "browser_download_url.*zip" | cut -d : -f 2,3 | tr -d \" | sed 's/^[ \t]*//')
+wget "$INTER_ZIP_URL" -O Inter.zip
+
+unzip Inter.zip -d InterFont
+find InterFont -maxdepth 2 -type f -name "*.ttf" -exec cp {} ~/.local/share/fonts/ \;
+find InterFont -maxdepth 2 -type f -name "*.ttc" -exec cp {} ~/.local/share/fonts/ \;
+
+rm -rf Inter.zip InterFont
+
 fc-cache
-cd -
+cd - || return
